3d4b8ec36a2249ca1a212c8a1c28c717f5113097
Andreas <info@andreas-heine.net>
2020-06-06 14:01:43 +0200

    update

--- ws_client.py
+++ ws_client.py
@@ -1,10 +1,11 @@
 import asyncio, websockets
+from datetime import datetime
 
 async def request():
     uri = "ws://127.0.0.1:8000"
     async with websockets.connect(uri) as websocket:
         while 1:
             msg = await websocket.recv()
-            print(msg)
+            print(datetime.now(), msg)
 
 asyncio.get_event_loop().run_until_complete(request())
